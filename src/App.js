5c66e55ba28c8b4e4734381af2788e1b535da60b
Cameron Garner <camerongarner9@gmail.com>
2020-10-08 12:16:33 -0400

    finished styling the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, {useState,useEffect} from "react";
 import axios from "axios"
 import "./App.css";
+import styled from 'styled-components'
 
 function App() {
   let [spacepic,setspacepic] = useState("")
   let [spacepictitle,setspacepictitle] = useState("")
   let [spacepicdate,setspacepicdate] = useState("")
   let [spacepicexplanation,setspacepicexplanation] = useState("")
+
+  let Div1 = styled.div`
+background: gray;
+width: 100%;
+display:flex;
+flex-direction:column;
+align-items:center;
+  `
+let Img1 = styled.img`
+margin-bottom:3%;
+`
+let P1 = styled.p`
+margin-bottom:3%;
+font-size:1.75rem;
+text-align:center;
+`
+
   useEffect(function(){
     axios.get("https://api.nasa.gov/planetary/apod?api_key=HYjvljQGU4Q7qgcbRpLbnyhmAHfELgMpAlWVaNeD")
     .then(function(nasadata){
@@ -18,12 +36,12 @@ function App() {
     })
   },[])
   return (
-    <div>
+    <Div1>
       <h1>{spacepictitle}</h1>
   <h2>{spacepicdate}</h2>
-     <img src={spacepic}/> 
-  <p>{spacepicexplanation}</p>
-    </div>
+     <Img1 src={spacepic}/> 
+  <P1>{spacepicexplanation}</P1>
+    </Div1>
   
   )
 }
